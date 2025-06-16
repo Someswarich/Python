c94e863b58b72025d94f6eb033895e036ade0d94
Someswarich <someswari488@gmail.com>
2025-06-16 23:06:52 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,109 @@
+#1//Write a program to check if a number is even or odd.
+# a=13
+# if a%2==0:
+#     print("even")
+# else:
+#     print("odd")
+
+
+#2//Write a program to check if a number is positive, negative, or zero.
+
+# a=0
+# if a>0:
+#     print("positive")
+# elif a<0:
+#     print("negative")
+# else:
+#     print("zero")
+
+
+#3//Write a program to check if a number is divisible by both 3 and 5.
+# a=int(input("enter a number :"))
+# if a%3==0 and a%5==0:
+#     print("Number divisible by 3 & 5")
+# elif a%3==0 and a%5!=0:
+#     print("Divisible by 3")
+# elif a%3!=0 and a%5==0:
+#     print("Divisible by 5")
+# else:
+#     print("Not Divisible by 3 & 5")
+
+# 4//Write a program to find the maximum of two numbers.
+# a=int(input("enter a number :"))
+# b=int(input("enter a number :"))
+# if a>b:
+#   print(a, "is greater than ",b)
+# else:
+#   print(a,"less than",b)
+
+# 5//Write a program to check if a year is a leap year.
+# a=int(input("enter a number :"))
+# if a%4==0:
+#     print("Leap year")
+# else:
+#     print("not a leap year")
+ #6//Write a program to determine if a person is eligible to vote (age ≥ 18).
+# a=int(input("enter a number :"))
+# if a>=18:
+#     print("Eligible for vote")
+# else:
+#     print("Not Eligible for vote")
+
+#7//Write a program to assign a grade based on marks:
+#A (90–100), B (75–89), C (50–74), Fail (<50)
+# a=int(input("enter a number :"))
+# if a>=90:
+#     print("A grade")
+# elif a>=75:
+#     print("B Grade")
+# elif a>=50:
+#     print("C grade")
+# else:
+#     print("Fail")
+
+    
+#A 8//triangle is considered valid if the sum of any two of its sides is greater than the third side
+#Write a program to check whether a triangle is valid based on its sides.
+# a=int(input("enter a number :"))
+# b=int(input("enter a number :"))
+# c=int(input("enter a number :"))
+# if (a+b>=c) and (a+c>=b) and (c+b>=a):
+#     print("it is a triangle")
+# else:
+#     print("not a triangle")
+
+# 9//Write a program to check if a number is within the range 10 to 50 (inclusive).
+# a=int(input("enter a number :"))
+# if 10 <= a <= 50:
+#     print("is in range")
+
+
+ #10//Write a program to check if a number is a three-digit number (i.e., from 100 to 999 or -100 to -999).
+# a=int(input("enter a number :"))
+# if 100<=a<=999:
+#     print("It is in range of 100 to 999")
+# elif -100<=a<=-999:
+#     print("It is in range of -100 to -999")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
